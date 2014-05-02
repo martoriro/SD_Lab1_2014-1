0dc3b128d7e4055fc592b53286d38985db05b1f3
Gustavo Salvo <gsalvo@vasal.cl>
2014-05-01 23:49:49 -0400

    ejemplo agregar función a interfaz

--- SchoolServer/src/interfaceServer/InterfaceS.java
+++ SchoolServer/src/interfaceServer/InterfaceS.java
@@ -4,8 +4,10 @@
  */
 package interfaceServer;
 
+import entityClass.Anotacion;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 /**
  *
@@ -13,4 +15,5 @@ import java.rmi.RemoteException;
  */
 public interface InterfaceS extends Remote{
     public boolean userLogin (String user, String pass) throws RemoteException;
+    public List<Anotacion> filtrarAnotaciones(String rut, String tipo) throws RemoteException;
 }
--- SchoolServer/src/viewServer/test.java
+++ SchoolServer/src/viewServer/test.java
@@ -32,11 +32,11 @@ public class test extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 364, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 223, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
